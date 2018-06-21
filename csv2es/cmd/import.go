b32aaf2f6d4661c13a5ea4ac3cc30edb465f596b
alex <cyy0523xc@gmail.com>
2018-06-21 18:23:51 +0800

    first build

---
+++ csv2es/cmd/import.go
@@ -0,0 +1,140 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/ibbd-dev/go-csv"
+	"github.com/spf13/cobra"
+	"gopkg.in/olivere/elastic.v5"
+)
+
+// importCmd represents the import command
+var importCmd = &cobra.Command{
+	Use:   "import",
+	Short: "import data from csv to es",
+	Long: `import data from csv to es
+`,
+	Example: `
+csv2es import --host=locahost --port=9200 --mapping=mapping_filename.json --index=test --type=test --csv=source.csv
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// 创建输出文件
+		in, err := os.Open(cParams.CsvFilename)
+		if err != nil {
+			panic(err)
+		}
+		defer in.Close()
+
+		reader := goCsv.NewMapReader(in)
+		fieldnames, err := reader.GetFieldnames()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("fieldnames: %s\n", strings.Join(fieldnames, ", "))
+
+		// es
+		conn, err := getESConnect()
+		ctx := context.Background()
+		exists, err := conn.IndexExists(cParams.IndexName).Do(ctx)
+		if err != nil {
+			panic(fmt.Errorf("check index exists error: %v", err.Error()))
+		}
+
+		if exists {
+			if cParams.DeleteIndex {
+				// 删除同名索引
+				if _, err = conn.DeleteIndex(cParams.IndexName).Do(ctx); err != nil {
+					panic(fmt.Errorf("delete index error: %s", cParams.IndexName))
+				}
+			}
+		} else {
+			// 创建索引
+			conn.CreateIndex(cParams.IndexName).Do(ctx)
+
+			if len(cParams.Mapping) > 0 {
+				bytes, err := ioutil.ReadFile(cParams.Mapping)
+				if err != nil {
+					fmt.Println("Read mapping file: ", err.Error())
+					panic(err)
+				}
+
+				var mapping = make(map[string]interface{})
+				if err := json.Unmarshal(bytes, &mapping); err != nil {
+					fmt.Println("Json Unmarshal: ", err.Error())
+					panic(err)
+				}
+
+				conn.PutMapping().Index(cParams.IndexName).BodyJson(mapping).Do(ctx)
+			}
+		}
+
+		bulk := conn.Bulk()
+		for {
+			row, err := reader.Read()
+			if err == io.EOF {
+				fmt.Printf("the file is over")
+				break
+			}
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Row 1: %+v\n", row)
+
+			req := elastic.NewBulkIndexRequest().Index(cParams.IndexName).Type(cParams.DocType).Doc(row)
+			bulk.Add(req)
+		}
+
+		// 执行导入
+		bulkResponse, err := bulk.Do(ctx)
+		if err != nil {
+			panic(fmt.Errorf("index %v 批量导入数据出错: %v", cParams.IndexName, err.Error()))
+		}
+
+		// 统计写入状态
+		var count int
+		indexed := bulkResponse.Indexed()
+		for i, res := range indexed {
+			if res.Error != nil {
+				fmt.Printf("ERROR: %d, %+v\n", i, res.Error)
+				count++
+			}
+		}
+		fmt.Printf("向es写入的数据量：%d，异常：%d", len(indexed), count)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(importCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// importCmd.PersistentFlags().String("foo", "", "A help for foo")
+	importCmd.PersistentFlags().BoolVar(&cParams.DeleteIndex, "delete-index", false, "delete the same index before")
+	importCmd.PersistentFlags().StringVar(&cParams.Mapping, "mapping", "", "the mapping file name, json format")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// importCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ csv2es/cmd/root.go
@@ -0,0 +1,94 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	"gopkg.in/olivere/elastic.v5"
+)
+
+type CommonParams struct {
+	// es config
+	Host      string
+	Port      int
+	IndexName string
+	DocType   string
+
+	// import
+	DeleteIndex bool   // 是否删除原索引
+	Mapping     string // mapping 文件
+
+	CsvFilename string
+}
+
+var cParams CommonParams
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:     "csv2es",
+	Version: "v1.0",
+	Short:   "import/export data beteen csv and es",
+	Long: `import/export data between csv and es
+
+Author:  Alex Cai
+BuildAt: 20180621
+`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&cParams.Host, "host", "localhost", "es host")
+	rootCmd.PersistentFlags().IntVar(&cParams.Port, "port", 9200, "es port")
+	rootCmd.PersistentFlags().StringVar(&cParams.IndexName, "index", "", "es index name")
+	rootCmd.PersistentFlags().StringVar(&cParams.DocType, "type", "", "es doc type")
+	rootCmd.PersistentFlags().StringVar(&cParams.CsvFilename, "csv", "", "csv filename")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+}
+
+func getESConnect() (*elastic.Client, error) {
+	return elastic.NewClient(
+		elastic.SetURL(fmt.Sprintf("http://%s:%d", cParams.Host, cParams.Port)),
+		elastic.SetSniff(false),
+		elastic.SetHealthcheckInterval(10*time.Second),
+		elastic.SetMaxRetries(5),
+	)
+}
